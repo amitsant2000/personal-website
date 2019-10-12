60144b4862f3347899168547272b69f69d09cb81
Amit Sant <amitsant.2000@gmail.com>
2019-10-11 17:46:21 -0700

    guesss we are using node now!

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ express = include('express')
 var app = express()
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.get('/', function(res,req,next){
+  res.render('index')
+});
