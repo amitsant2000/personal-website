14fda8d3ca047b3ee3ea2277dd879ec5eafe3c45
Amit Sant <amitsant.2000@gmail.com>
2019-10-13 22:35:32 -0700

    pls work

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+var createError = require('http-errors');
 express = include('express')
+var path = require('path');
 var app = express()
+var http = require('http');
+var port = normalizePort(process.env.PORT || '3001');
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.get('/', function(res,req,next){
-  res.render('index')
+  res.render('index');
 });
