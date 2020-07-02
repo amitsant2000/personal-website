a0a5cfd20367ab70f1a754f6e4a1f57e8b9a3d88
Amit Sant <amitsant2000@berkeley.edu>
2020-07-01 18:14:30 -0700

    resume added

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var cookieParser = require('cookie-parser');
 var port = process.env.PORT || '5000';
 app.set('port', port);
 var server = http.createServer(app);
+var fs = require('fs');
 server.listen(port);
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,28 +21,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.get('/', function(req,res,next){
+app.get('/', function(req, res, next){
   res.render('index', {title: 'Home'})
 });
-app.get('/about', function(req,res,next){
+app.get('/about', function(req, res, next){
   res.render('about', {title: 'About'})
 });
-app.get('/contact', function(req,res,next){
+app.get('/contact', function(req, res, next){
   res.render('contact', {title: 'Contact Me'})
 });
-app.get('/projects', function(req,res,next){
+app.get('/projects', function(req, res, next){
   res.render('projects', {title: 'Projects'})
 });
-app.post('/contact',function(req,res,next){
-  let q = req.body
-  let data = {
-    'from': 'Mailgun Sandbox <postmaster@sandbox35589a5931cb4227bbfd40d053a58292.mailgun.org>',
-    'to': 'amitsant2000@berkeley.edu',
-    'subject': q.fullname + ' - ' + q.email + ' - ' + q.subject,
-    'text': q.body
-  }
-  mg.messages().send(data, function (error, body) {
-  	console.log(body);
-    res.redirect('/contact')
-  });
-})
